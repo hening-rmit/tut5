2cf58a9c5faf304dd721f14242bdc82dbaab056b
hening-rmit <128437237+hening-rmit@users.noreply.github.com>
2023-08-22 22:28:53 +1000

    Update Circle.java

--- graphics/Circle.java
+++ graphics/Circle.java
@@ -2,10 +2,10 @@ package tut5.graphics;
 
 public class Circle extends Shape {
     public double getRadious() {
-        return 0;
+        return 0.0;
     }
 
     public double getPerimeter() {
-        return 0;
+        return 0.0;
     }
 }
